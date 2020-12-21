99e65ec918a04b0f7ca92cb0ee9b1234142eff93
Alexander <alex.chipliev@gmail.com>
2020-12-21 05:28:44 +0400

    beta 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,7 +56,7 @@ module.exports = {
       options: {
         name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
         short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        description: `Привет! Это мой блог. Здесь я пишу на разные темы, в том числе о разработке сайтов, веб-технологиях, программировании и о многом другом! Блог работает в офлайне, так что ты можешь читать статьи даже если у тебя отключится Интернет :)`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#6B46C1`,
